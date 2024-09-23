5aaba1f3eb0431ff2a9cd37eb09beb4bed78410b
Arina Levchuk <>
2024-09-23 14:48:28 +0200

    [T2_1] reverseBinary

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    
+    for (int bitIndex = 0; bitIndex < 8; bitIndex++ ) {
+        result <<= 1; // сдвиг влево - освобождаем место для нового бита
+        result |= (n & 1); // берем младший бит у n
+        n >>= 1; // удаляем обработанный младший бит сдвигом вправо
+    }
+    
+    return result;
 }
