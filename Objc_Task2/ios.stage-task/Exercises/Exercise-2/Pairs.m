e9d7e54dafbd1732109ecc12f08e8122a15328d2
Arina Levchuk <>
2024-09-24 11:32:27 +0200

    [T2_2] Pairs

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger pairCount = 0;
+    NSInteger deltaValue = number.integerValue;
+    NSSet *set = [NSSet setWithArray:array];
+    
+    for (NSNumber *value in array) {
+        NSInteger targetValue = value.integerValue + deltaValue;
+        if ([set containsObject:@(targetValue)]) {
+            pairCount++;
+        }
+    }
+    
+    return pairCount;
 }
 
 @end
